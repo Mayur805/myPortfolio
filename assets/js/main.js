8973f5f080dccf849e347e83963ae7b6d97b4aac
mayur <mayurparmar84018@gmail.com>
2023-01-12 23:27:59 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.menu-social').click(function() {
+        $('.iconssss').toggle();
+    });
+    // $('.menu-social').mouseout(function() {
+    //     $('.iconssss').css('display','none');
+    // });
+})
